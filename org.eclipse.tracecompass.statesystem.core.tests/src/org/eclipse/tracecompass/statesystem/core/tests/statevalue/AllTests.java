fd94a670165c17bf6b9703c6ca4cf152abcc632e
Naser Ezzati <n.ezzati@polymtl.ca>
2014-09-22 15:23:08 -0400

    State system: add a Compare() method to ITmfStateValue
    It is required to compare two state values based on different comparison
    operators. It will be useful for the XML analysis.
    
    Change-Id: I1fcb9525a41b11a648922271b5c2e06d6bd309ac
    Signed-off-by: Naser Ezzati <n.ezzati@polymtl.ca>

--- org.eclipse.tracecompass.statesystem.core.tests/src/org/eclipse/tracecompass/statesystem/core/tests/statevalue/AllTests.java
+++ org.eclipse.tracecompass.statesystem.core.tests/src/org/eclipse/tracecompass/statesystem/core/tests/statevalue/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 Ericsson
+ * Copyright (c) 2014 École Polytechnique de Montréal
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -7,21 +7,20 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Alexandre Montplaisir - Initial API and implementation
+ *   Naser Ezzati - Initial API and implementation
  *******************************************************************************/
 
-package org.eclipse.tracecompass.tmf.core.tests.statesystem;
+package org.eclipse.tracecompass.statesystem.core.tests.statevalue;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
- * Signal tests
+ * Test suite for org.eclipse.linuxtools.tmf.core.statesystem
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    ExperimentStateSystemModuleTest.class,
-    StateSystemAnalysisModuleTest.class
+        StateValueCompareToTest.class
 })
 public class AllTests {
 
