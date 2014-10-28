6915e09b4179817dd5615c8a568be3f63834607f
Geneviève Bastien <gbastien+lttng@versatic.net>
2014-10-28 16:45:30 -0400

    tmf: Have ITmfTrace#getHostId return type be NonNull
    This will avoid a lot of null checks in analysis
    
    Change-Id: Ic5730cab105ac9652dde4dc71a4524d946cbaf59
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>

--- org.eclipse.tracecompass.tmf.analysis.xml.core/src/org/eclipse/tracecompass/tmf/analysis/xml/core/stateprovider/XmlStateSystemModule.java
+++ org.eclipse.tracecompass.tmf.analysis.xml.core/src/org/eclipse/tracecompass/tmf/analysis/xml/core/stateprovider/XmlStateSystemModule.java
@@ -45,21 +45,24 @@ public class XmlStateSystemModule extends TmfStateSystemAnalysisModule {
 
     @Override
     public String getName() {
-        String name = getId();
+        String id = getId();
         IPath xmlFile = fXmlFile;
         if (xmlFile == null) {
-            return name;
+            return id;
         }
         Element doc = XmlUtils.getElementInFile(xmlFile.makeAbsolute().toString(), TmfXmlStrings.STATE_PROVIDER, getId());
         /* Label may be available in XML header */
         List<Element> head = XmlUtils.getChildElements(doc, TmfXmlStrings.HEAD);
+        String name = id;
         if (head.size() == 1) {
             List<Element> labels = XmlUtils.getChildElements(head.get(0), TmfXmlStrings.LABEL);
             if (!labels.isEmpty()) {
                 name = labels.get(0).getAttribute(TmfXmlStrings.VALUE);
             }
         }
-
+        if (name == null) {
+            return id;
+        }
         return name;
     }
 
