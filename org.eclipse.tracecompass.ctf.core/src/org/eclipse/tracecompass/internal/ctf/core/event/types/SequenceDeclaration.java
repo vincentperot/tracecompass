bee0ddd5a5c2593fcea17a64cfa8442532c6dd22
Matthew Khouzam <matthew.khouzam@ericsson.com>
2014-11-06 15:24:16 -0500

    ctf: add equals and hashcodes to Declarations
    Change-Id: I15d8030d2f75979012b87caae4dfc08bbda3ee7a
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.tracecompass.ctf.core/src/org/eclipse/tracecompass/internal/ctf/core/event/types/SequenceDeclaration.java
+++ org.eclipse.tracecompass.ctf.core/src/org/eclipse/tracecompass/internal/ctf/core/event/types/SequenceDeclaration.java
@@ -142,8 +142,7 @@ public class SequenceDeclaration extends CompoundDeclaration {
             definitions.add(fElemType.createDefinition(definitionScope, elemName, input));
         }
         @SuppressWarnings("null")
-        @NonNull
-        ImmutableList<Definition> build = definitions.build();
+        @NonNull ImmutableList<Definition> build = definitions.build();
         return new ArrayDefinition(this, definitionScope, fieldName, build);
     }
 
@@ -156,6 +155,40 @@ public class SequenceDeclaration extends CompoundDeclaration {
     @Override
     public int getMaximumSize() {
         return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + fElemType.hashCode();
+        result = prime * result + fLengthName.hashCode();
+        result = prime * result + fPaths.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SequenceDeclaration other = (SequenceDeclaration) obj;
+        if (!fElemType.equals(other.fElemType)) {
+            return false;
+        }
+        if (!fLengthName.equals(other.fLengthName)) {
+            return false;
+        }
+        if (!fPaths.equals(other.fPaths)) {
+            return false;
+        }
+        return true;
     }
 
 }
