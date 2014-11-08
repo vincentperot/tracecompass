f022515be10265455969f50b5fddd29eabb0eb07
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2014-11-08 11:09:13 +0100

    lttng: Fix system calls for LTTng 2.6 kernel traces
    The "syscall entry" and "syscall exit" events were renamed in LTTng 2.6.
    This patch adds a new trace layout, which extends the base LTTng definitions,
    but updates the system call event names.
    
    "syscall exit" also had to be changed to a prefix, because LTTng 2.6 now lists
    the syscall name in exits too. We don't make use of it though, so it shouldn't
    affect other tracers who don't.
    
    Also required updating the CFV/RV to only display the non-prefix part of the
    syscall name, no matter what layout is used underneat.
    
    Change-Id: I9061b0dff92daa11894d951c09ed3dffe80e4d1b
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

--- org.eclipse.tracecompass.lttng2.kernel.core/src/org/eclipse/tracecompass/internal/lttng2/kernel/core/trace/layout/LttngEventLayout.java
+++ org.eclipse.tracecompass.lttng2.kernel.core/src/org/eclipse/tracecompass/internal/lttng2/kernel/core/trace/layout/LttngEventLayout.java
@@ -17,8 +17,8 @@ import java.util.Collection;
 import com.google.common.collect.ImmutableList;
 
 /**
- * This file defines all the known event and field names for LTTng 2.0 kernel
- * traces.
+ * This file defines all the known event and field names for LTTng kernel
+ * traces, for versions of lttng-modules up to 2.5.
  *
  * These should not be externalized, since they need to match exactly what the
  * tracer outputs. If you want to localize them in a view, you should do a
@@ -30,7 +30,6 @@ import com.google.common.collect.ImmutableList;
 public class LttngEventLayout implements IKernelAnalysisEventLayout {
 
     /* Event names */
-    private static final String EXIT_SYSCALL = "exit_syscall";
     private static final String IRQ_HANDLER_ENTRY = "irq_handler_entry";
     private static final String IRQ_HANDLER_EXIT = "irq_handler_exit";
     private static final String SOFTIRQ_ENTRY = "softirq_entry";
@@ -46,8 +45,9 @@ public class LttngEventLayout implements IKernelAnalysisEventLayout {
     private static final String SCHED_PROCESS_FREE = "sched_process_free";
     private static final String STATEDUMP_PROCESS_STATE = "lttng_statedump_process_state";
 
-    private static final String SYSCALL_PREFIX = "sys_";
-    private static final String COMPAT_SYSCALL_PREFIX = "compat_sys_";
+    private static final String SYSCALL_ENTRY_PREFIX = "sys_";
+    private static final String COMPAT_SYSCALL_ENTRY_PREFIX = "compat_sys_";
+    private static final String SYSCALL_EXIT_PREFIX = "exit_syscall";
 
     /* Field names */
     private static final String IRQ = "irq";
@@ -62,7 +62,7 @@ public class LttngEventLayout implements IKernelAnalysisEventLayout {
     private static final String CHILD_TID = "child_tid";
 
     /** All instances are the same. Only provide a static instance getter */
-    private LttngEventLayout() {
+    protected LttngEventLayout() {
     }
 
     private static final IKernelAnalysisEventLayout INSTANCE = new LttngEventLayout();
@@ -137,19 +137,20 @@ public class LttngEventLayout implements IKernelAnalysisEventLayout {
     public String eventStatedumpProcessState() {
         return STATEDUMP_PROCESS_STATE;
     }
-    @Override
-    public String eventSyscallExit() {
-        return EXIT_SYSCALL;
-    }
 
     @Override
     public String eventSyscallEntryPrefix() {
-        return SYSCALL_PREFIX;
+        return SYSCALL_ENTRY_PREFIX;
     }
 
     @Override
     public String eventCompatSyscallEntryPrefix() {
-        return COMPAT_SYSCALL_PREFIX;
+        return COMPAT_SYSCALL_ENTRY_PREFIX;
+    }
+
+    @Override
+    public String eventSyscallExitPrefix() {
+        return SYSCALL_EXIT_PREFIX;
     }
 
     // ------------------------------------------------------------------------
