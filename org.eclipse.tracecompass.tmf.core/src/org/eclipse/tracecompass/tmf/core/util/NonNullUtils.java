cb5fd6a0f2cb13ebc592370f001101b16be2585c
Alexandre Montplaisir <alexmonthy@voxpopuli.im>
2014-12-02 08:58:28 -0500

    tmf: Add utility class for non-null things
    Change-Id: I8c1aa8469dc466a1b42aae799b24789dc23837cf
    Signed-off-by: Alexandre Montplaisir <alexmonthy@voxpopuli.im>

---
+++ org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/util/NonNullUtils.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Alexandre Montplaisir - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.tmf.core.util;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Utility methods to handle {@link org.eclipse.jdt.annotation.NonNull}
+ * annotations.
+ *
+ * @author Alexandre Montplaisir
+ */
+@NonNullByDefault
+public class NonNullUtils {
+
+    private NonNullUtils() {}
+
+    /**
+     * Convert a potentially null string into an empty one if it is null.
+     *
+     * @param str
+     *            The string to null-check, and convert to an empty string if
+     *            null.
+     * @return The non-null string
+     */
+    public static String nullToEmptyString(@Nullable String str) {
+        if (str == null) {
+            return ""; //$NON-NLS-1$
+        }
+        return str;
+    }
+}
