5d379e9ba4895801f352eccb1c765275df7a7a21
Matthew Khouzam <matthew.khouzam@ericsson.com>
2014-12-03 15:05:57 -0500

    xml analysis: fix warning
    Change-Id: I0f700c9c96de12470539106d24868e2c7304c904
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.tracecompass.tmf.analysis.xml.core/src/org/eclipse/tracecompass/tmf/analysis/xml/core/model/readonly/TmfXmlReadOnlyStateValue.java
+++ org.eclipse.tracecompass.tmf.analysis.xml.core/src/org/eclipse/tracecompass/tmf/analysis/xml/core/model/readonly/TmfXmlReadOnlyStateValue.java
@@ -15,6 +15,7 @@ package org.eclipse.tracecompass.tmf.analysis.xml.core.model.readonly;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.tracecompass.tmf.analysis.xml.core.model.ITmfXmlStateAttribute;
 import org.eclipse.tracecompass.tmf.analysis.xml.core.model.TmfXmlStateValue;
 import org.eclipse.tracecompass.tmf.analysis.xml.core.module.IXmlStateSystemContainer;
@@ -63,7 +64,13 @@ public class TmfXmlReadOnlyStateValue extends TmfXmlStateValue {
      */
     public TmfXmlReadOnlyStateValue(TmfXmlReadOnlyModelFactory modelFactory, Element node,
             IXmlStateSystemContainer container, String eventField) {
-        super(modelFactory, node, container, Collections.EMPTY_LIST, eventField);
+        super(modelFactory, node, container, nullList(), eventField);
+    }
+
+    private static List<ITmfXmlStateAttribute> nullList() {
+        @SuppressWarnings("null")
+        @NonNull List<ITmfXmlStateAttribute> emptyList = Collections.<ITmfXmlStateAttribute>emptyList();
+        return emptyList;
     }
 
 }
