67edf9118e8df3d075dc39d8d2c9fe9fa55e5999
Patrick Tasse <patrick.tasse@gmail.com>
2014-12-18 12:22:21 -0500

    tmf: Support aspect-based filtering in FilterViewer
    Change-Id: I260e86d331130b060c96097aa852b76b8ef2e975
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>

--- org.eclipse.tracecompass.tmf.pcap.core/src/org/eclipse/tracecompass/internal/tmf/pcap/core/trace/PcapTrace.java
+++ org.eclipse.tracecompass.tmf.pcap.core/src/org/eclipse/tracecompass/internal/tmf/pcap/core/trace/PcapTrace.java
@@ -66,6 +66,9 @@ import com.google.common.collect.ImmutableMap;
  */
 public class PcapTrace extends TmfTrace implements ITmfEventParser, ITmfTraceProperties, AutoCloseable {
 
+    /** pcap trace type id as defined in plugin.xml */
+    public static final String TRACE_TYPE_ID = "org.eclipse.linuxtools.tmf.pcap.core.pcaptrace"; //$NON-NLS-1$
+
     private static final Collection<ITmfEventAspect> PCAP_ASPECTS =
             checkNotNull(ImmutableList.of(
                     ITmfEventAspect.BaseAspects.TIMESTAMP,
