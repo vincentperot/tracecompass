eclipse-linuxtools/org.eclipse.linuxtools

[lttng control] Use List<String> for control commands (Bug 455382)
By doing this, it's not necessary to surround commands with wildcards
and filters with quotes which made these commands fail using
org.eclipse.remote/Built-in SSH and org.eclipse.remote.Local.

For the PTP RSE adapter for org.eclipse.remote, the user has to enter
quotes otherwise the command will fail. The UI was changed to allow
quotes in the text boxes.

Change-Id: I2185c5b658f635ff491a6022fab206c208ca5d8f
Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>
Reviewed-on: https://git.eclipse.org/r/38578
Tested-by: Hudson CI
Reviewed-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
Tested-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

lttng/org.eclipse.linuxtools.lttng2.control.ui.tests/stubs/org/eclipse/linuxtools/internal/lttng2/control/stubs/shells/LTTngToolsFileShell.java
@@ -29,6 +29,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.linuxtools.internal.lttng2.control.ui.views.remote.CommandResult;
 import org.eclipse.linuxtools.internal.lttng2.control.ui.views.remote.ICommandResult;
+import org.eclipse.linuxtools.internal.lttng2.control.ui.views.service.LTTngControlService;
 
 @SuppressWarnings("javadoc")
 public class LTTngToolsFileShell extends TestCommandShell {
@@ -47,7 +48,7 @@ public class LTTngToolsFileShell extends TestCommandShell {
     private final static String ERROR_OUTPUT_END_KEY = "</COMMAND_ERROR_OUTPUT>";
     private final static String COMMENT_KEY = "#.*";
 
-    private final static Pattern LTTNG_LIST_SESSION_PATTERN =  Pattern.compile("lttng\\s+list\\s+(.+)");
+    private final static Pattern LTTNG_LIST_SESSION_PATTERN = Pattern.compile("lttng\\s+list\\s+(.+)");
     private final static String LTTNG_LIST_PROVIDER_PATTERN = "lttng\\s+list\\s+(-u|-k)";
 
     // ------------------------------------------------------------------------
@@ -61,6 +62,7 @@ public class LTTngToolsFileShell extends TestCommandShell {
 
     /**
      * Parse a scenario file with the format:
+     *
      * <pre>
      * &lt;SCENARIO&gt;
      * ScenarioName
@@ -90,7 +92,8 @@ public class LTTngToolsFileShell extends TestCommandShell {
      *
      * Note: 1) There can be many scenarios per file
      *       2) There can be many (Command-CommandResult-CommandOutput) triples per scenario
-     *       3) Lines starting with # will be ignored (comments)
+     * 3) Lines starting with # will be ignored (comments)
+     *
      * <pre>
      * @param scenariofile - path to scenario file
      * @throws Exception
@@ -236,12 +239,13 @@ public class LTTngToolsFileShell extends TestCommandShell {
     }
 
     @Override
-   public synchronized ICommandResult executeCommand(String command, IProgressMonitor monitor) throws ExecutionException {
+    public synchronized ICommandResult executeCommand(List<String> command, IProgressMonitor monitor) throws ExecutionException {
         Map<String, ICommandResult> commands = fScenarioMap.get(fScenario);
-        String fullCommand = command;
+        String commandLine = LTTngControlService.toCommandString(command);
+        String fullCommand = commandLine;
 
-        Matcher matcher = LTTNG_LIST_SESSION_PATTERN.matcher(command);
-        if (matcher.matches() && !command.matches(LTTNG_LIST_PROVIDER_PATTERN)) {
+        Matcher matcher = LTTNG_LIST_SESSION_PATTERN.matcher(commandLine);
+        if (matcher.matches() && !commandLine.matches(LTTNG_LIST_PROVIDER_PATTERN)) {
             String sessionName = matcher.group(1).trim();
             Integer i = fSessionNameMap.get(sessionName);
             if (i != null) {
@@ -265,7 +269,7 @@ public class LTTngToolsFileShell extends TestCommandShell {
         result.setErrorOutput(output);
         result.setResult(1);
         return result;
-   }
+    }
 
     // ------------------------------------------------------------------------
     // Helper methods

vincentperot/tracecompass

org.eclipse.tracecompass.lttng2.control.ui.tests/stubs/org/eclipse/tracecompass/internal/lttng2/control/stubs/shells/LTTngToolsFileShell.java
@@ -29,6 +29,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.views.remote.CommandResult;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.views.remote.ICommandResult;
+import org.eclipse.tracecompass.internal.lttng2.control.ui.views.service.LTTngControlService;
 
 @SuppressWarnings("javadoc")
 public class LTTngToolsFileShell extends TestCommandShell {
@@ -47,7 +48,7 @@ public class LTTngToolsFileShell extends TestCommandShell {
     private final static String ERROR_OUTPUT_END_KEY = "</COMMAND_ERROR_OUTPUT>";
     private final static String COMMENT_KEY = "#.*";
 
-    private final static Pattern LTTNG_LIST_SESSION_PATTERN =  Pattern.compile("lttng\\s+list\\s+(.+)");
+    private final static Pattern LTTNG_LIST_SESSION_PATTERN = Pattern.compile("lttng\\s+list\\s+(.+)");
     private final static String LTTNG_LIST_PROVIDER_PATTERN = "lttng\\s+list\\s+(-u|-k)";
 
     // ------------------------------------------------------------------------
@@ -61,6 +62,7 @@ public class LTTngToolsFileShell extends TestCommandShell {
 
     /**
      * Parse a scenario file with the format:
+     *
      * <pre>
      * &lt;SCENARIO&gt;
      * ScenarioName
@@ -90,7 +92,8 @@ public class LTTngToolsFileShell extends TestCommandShell {
      *
      * Note: 1) There can be many scenarios per file
      *       2) There can be many (Command-CommandResult-CommandOutput) triples per scenario
-     *       3) Lines starting with # will be ignored (comments)
+     * 3) Lines starting with # will be ignored (comments)
+     *
      * <pre>
      * @param scenariofile - path to scenario file
      * @throws Exception
@@ -236,12 +239,13 @@ public class LTTngToolsFileShell extends TestCommandShell {
     }
 
     @Override
-   public synchronized ICommandResult executeCommand(String command, IProgressMonitor monitor) throws ExecutionException {
+    public synchronized ICommandResult executeCommand(List<String> command, IProgressMonitor monitor) throws ExecutionException {
         Map<String, ICommandResult> commands = fScenarioMap.get(fScenario);
-        String fullCommand = command;
+        String commandLine = LTTngControlService.toCommandString(command);
+        String fullCommand = commandLine;
 
-        Matcher matcher = LTTNG_LIST_SESSION_PATTERN.matcher(command);
-        if (matcher.matches() && !command.matches(LTTNG_LIST_PROVIDER_PATTERN)) {
+        Matcher matcher = LTTNG_LIST_SESSION_PATTERN.matcher(commandLine);
+        if (matcher.matches() && !commandLine.matches(LTTNG_LIST_PROVIDER_PATTERN)) {
             String sessionName = matcher.group(1).trim();
             Integer i = fSessionNameMap.get(sessionName);
             if (i != null) {
@@ -265,7 +269,7 @@ public class LTTngToolsFileShell extends TestCommandShell {
         result.setErrorOutput(output);
         result.setResult(1);
         return result;
-   }
+    }
 
     // ------------------------------------------------------------------------
     // Helper methods