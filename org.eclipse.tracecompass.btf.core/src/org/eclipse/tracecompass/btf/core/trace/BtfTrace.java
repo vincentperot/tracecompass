96861c7ce0fe6e5356c4cbc517337a0ab6aa8f6a
Geneviève Bastien <gbastien+lttng@versatic.net>
2015-02-05 21:58:05 -0500

    TMF: Add some NonNull annotations to ITmfTimestampTransform
    And all the necessary NonNull annotations that this little change
    requires
    
    Change-Id: I4da75d6d936f0f1adbd3e5b27ca6182711557841
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>

--- org.eclipse.tracecompass.btf.core/src/org/eclipse/tracecompass/btf/core/trace/BtfTrace.java
+++ org.eclipse.tracecompass.btf.core/src/org/eclipse/tracecompass/btf/core/trace/BtfTrace.java
@@ -107,7 +107,7 @@ public class BtfTrace extends TmfTrace implements ITmfPersistentlyIndexable, ITm
     private String fCreationDate;
     private String fInputFile;
     // default unit is ns
-    private BtfTimstampFormat fTsFormat = BtfTimstampFormat.NS;
+    private BtfTimestampFormat fTsFormat = BtfTimestampFormat.NS;
 
     private File fFile;
     private RandomAccessFile fFileInput;
@@ -159,7 +159,7 @@ public class BtfTrace extends TmfTrace implements ITmfPersistentlyIndexable, ITm
                 fProperties.put(INPUTFILE, fInputFile);
                 break;
             case lTIMESCALE:
-                fTsFormat = BtfTimstampFormat.parse(tokens[1]);
+                fTsFormat = BtfTimestampFormat.parse(tokens[1]);
                 fProperties.put(TIMESCALE, fTsFormat.toString());
                 break;
             case lENTITYTYPE:
