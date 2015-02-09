fec85f7ac0f3a5daee77b00083fd25353c08da65
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-02-08 22:34:53 -0500

    TMF: Add the dependency graph and critical path analyses
    Adds a new plugin: org.eclipse.tracecompass.analysis.graph.core that contains
    the classes to
    
    * represent the dependency graph
    * build the graph from the events
    * compute the critical path from a dependency graph
    
    Change-Id: I8bd45263cbf02b42d62b44868409b50ac46125ac
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ org.eclipse.tracecompass.analysis.graph.core/src/org/eclipse/tracecompass/analysis/graph/core/base/TmfEdge.java
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * Copyright (c) 2015 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Geneviève Bastien and Francis Giraldeau - Initial implementation and API
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.analysis.graph.core.base;
+
+/**
+ * Edge of a TmfGraph
+ *
+ * @author Francis Giraldeau
+ * @author Geneviève Bastien
+ */
+public class TmfEdge {
+
+    /**
+     * Enumeration of the different types of edges
+     *
+     * FIXME: this sounds very specific to kernel traces, maybe it shouldn't be
+     * here
+     */
+    public enum EdgeType {
+
+        /**
+         * Special edge, so it is possible to have two vertices at the same
+         * timestamp
+         */
+        EPS,
+        /** Unknown edge */
+        UNKNOWN,
+        /** Default type for an edge */
+        DEFAULT,
+        /** Worker is running */
+        RUNNING,
+        /** Worker is blocked */
+        BLOCKED,
+        /** Worker is in an interrupt state */
+        INTERRUPTED,
+        /** Worker is preempted */
+        PREEMPTED,
+        /** In a timer */
+        TIMER,
+        /** Edge represents a network communication */
+        NETWORK,
+        /** Worker is waiting for user input */
+        USER_INPUT,
+        /** Block device */
+        BLOCK_DEVICE
+
+    }
+
+    private EdgeType fType;
+    private final TmfVertex fVertexFrom;
+    private final TmfVertex fVertexTo;
+
+    /**
+     * Constructor
+     *
+     * @param from
+     *            The vertex this edge leaves from
+     * @param to
+     *            The vertex the edge leads to
+     */
+    public TmfEdge(TmfVertex from, TmfVertex to) {
+        this.fVertexFrom = from;
+        this.fVertexTo = to;
+        this.fType = EdgeType.DEFAULT;
+    }
+
+    /*
+     * Getters
+     */
+
+    /**
+     * Get the origin vertex of this edge
+     *
+     * @return The origin vertex
+     */
+    public TmfVertex getVertexFrom() {
+        return fVertexFrom;
+    }
+
+    /**
+     * Get the destination vertex of this edge
+     *
+     * @return The destination vertex
+     */
+    public TmfVertex getVertexTo() {
+        return fVertexTo;
+    }
+
+    /**
+     * Get the edge type
+     *
+     * @return The type of the edge
+     */
+    public EdgeType getType() {
+        return fType;
+    }
+
+    /**
+     * Sets the edge type
+     *
+     * @param type
+     *            The edge type
+     */
+    public void setType(final EdgeType type) {
+        fType = type;
+    }
+
+    /**
+     * Returns the duration of the edge
+     *
+     * @return The duration (in nanoseconds)
+     */
+    public long getDuration() {
+        return fVertexTo.getTs() - fVertexFrom.getTs();
+    }
+
+    @SuppressWarnings("nls")
+    @Override
+    public String toString() {
+        return "[" + fVertexFrom + "--" + fType + "->" + fVertexTo + "]";
+    }
+}
