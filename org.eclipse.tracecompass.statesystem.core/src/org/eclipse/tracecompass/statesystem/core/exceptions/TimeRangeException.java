3a352e72a2ff41c2de8980daf92100024601a284
Patrick Tasse <patrick.tasse@gmail.com>
2015-02-16 18:32:47 -0500

    tmf: Add message to state system exceptions
    Change-Id: Ic8e75aed32690dae72ba2f907d1fc8ebc170de1e
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>

--- org.eclipse.tracecompass.statesystem.core/src/org/eclipse/tracecompass/statesystem/core/exceptions/TimeRangeException.java
+++ org.eclipse.tracecompass.statesystem.core/src/org/eclipse/tracecompass/statesystem/core/exceptions/TimeRangeException.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Ericsson
+ * Copyright (c) 2012, 2015 Ericsson
  * Copyright (c) 2010, 2011 École Polytechnique de Montréal
  * Copyright (c) 2010, 2011 Alexandre Montplaisir <alexandre.montplaisir@gmail.com>
  *
@@ -8,6 +8,9 @@
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
+ * Contributors:
+ *   Alexandre Montplaisir - Initial API and implementation
+ *   Patrick Tasse - Add message to exceptions
  *******************************************************************************/
 
 package org.eclipse.tracecompass.statesystem.core.exceptions;
@@ -27,4 +30,31 @@ public class TimeRangeException extends RuntimeException {
 
     private static final long serialVersionUID = -4067685227260254532L;
 
+    /**
+     * Default constructor
+     */
+    public TimeRangeException() {
+    }
+
+    /**
+     * Constructor with a message
+     *
+     * @param message
+     *            Message to attach to this exception
+     */
+    public TimeRangeException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor with both a message and a cause.
+     *
+     * @param message
+     *            Message to attach to this exception
+     * @param e
+     *            Cause of this exception
+     */
+    public TimeRangeException(String message, Throwable e) {
+        super(message, e);
+    }
 }
