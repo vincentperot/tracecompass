5914fbcaeda02c509d83f33bccde1c0cb7a5a056
Matthew Khouzam <matthew.khouzam@ericsson.com>
2015-02-16 22:49:50 -0500

    tmf.ui: introducing Regions of interest view
    this is a table that shows time ranges and lets you seek to an event
    or a range
    
    Change-Id: I02640b89b1a6096eb3b01da8a17b94a0bc6827d8
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

---
+++ org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/timerange/Messages.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Matthew Khouzam - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.tracecompass.tmf.ui.views.timerange;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ *  Timerange table strings
+ */
+public class Messages extends NLS {
+    private static final String BUNDLE_NAME = "org.eclipse.tracecompass.tmf.ui.views.timerange.messages"; //$NON-NLS-1$
+    /** content */
+    public static String RegionsOfInterestTable_content;
+    /** duration */
+    public static String RegionsOfInterestTable_duration;
+    /** end time */
+    public static String RegionsOfInterestTable_endTime;
+    /** start time */
+    public static String RegionsOfInterestTable_startTime;
+    /** import */
+    public static String RegionsOfInterestView_import;
+    /** import tooltip */
+    public static String RegionsOfInterestView_importToolTip;
+    /** paste */
+    public static String RegionsOfInterestView_Paste;
+    /** instructions label for dialog */
+    public static String TimeRangeInputDialog_label;
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+    }
+}
