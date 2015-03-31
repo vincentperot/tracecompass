03bcf14ced16b7229b84f631cfc23dec0f3ee993
Lrouge <sebastien.lorrain01@gmail.com>
2015-03-31 14:45:43 -0400

    Base code for Container Analysis
    Contains several new classes as well as a modification of LttngEventLayout
    
    LttngEventLayout modifications :
    The LTTng tracer was modified and fields for namespace/containers analysis were added for the following events :
    sched_process_fork, lttng_statedump_process_state
    Therefore, LttngEventLayout was modified to be able to get those new fields.
    The modification to the LTTng trace is pending, but a patch is available here :
    https://github.com/Selora/lttng-modules/tree/patch
    
    The rest of this commit only contains new classes for the Container Analysis.
    To run the analysis on Trace Compass, one need to import the containerView.xml file.
    Several traces with different use-cases are available there :
    https://github.com/Selora/lttng_container_traces
    
    To test the new analysis, a complete testbench will follow shortly
    
    Signed-off-by: Sebastien Lorrain <sebastien.lorrain01@gmail.com>
    Change-Id: Idf45b34c349faf4f26c564faa86a81445379b329

---
+++ org.eclipse.tracecompass.lttng2.kernel.core/src/org/eclipse/tracecompass/internal/lttng2/kernel/core/analysis/container/TaskContainerEvent.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Sébastien Lorrain - Initial API and implementation
+ *   Francis Jolivet - Initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.tracecompass.internal.lttng2.kernel.core.analysis.container;
+
+/**
+ * Represent a task that changed the CPU state at a given time. Also contains
+ * information about the PID namespace of that tasks
+ *
+ * @author Sebastien Lorrain
+ * @author Francis Jolivet
+ */
+public class TaskContainerEvent extends TaskCPUEvent {
+
+    private final int fVtid;
+    private final int fPpid;
+    private final int fNsInode;
+
+    /**
+     * Constructor
+     *
+     * @param ts
+     *            The timestamp
+     * @param tid
+     *            The task tid
+     * @param vTID
+     *            The task VTID
+     * @param pPID
+     *            The task parent pid
+     * @param status
+     *            The CPU status occasionned by the task
+     * @param cpuID
+     *            The CPU numerical ID
+     * @param nsInode
+     *            The INode of the task PID namespace
+     */
+    public TaskContainerEvent(long ts, int tid, int vTID, int pPID, int status, int cpuID, int nsInode) {
+        super(ts, tid, status, cpuID);
+        fVtid = vTID;
+        fPpid = pPID;
+        fNsInode = nsInode;
+    }
+
+
+    /**
+     * @return
+     *      The VTID of the TaskEvent
+     */
+    public int getVtid() {
+        return fVtid;
+    }
+
+
+    /**
+     * @return
+     *      The Parent PID of the TaskEvent
+     */
+    public int getPpid() {
+        return fPpid;
+    }
+
+
+    /**
+     * @return
+     *      The INode of the TaskEvent
+     */
+    public int getNsInode() {
+        return fNsInode;
+    }
+}
