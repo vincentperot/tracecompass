d8ef3829784a0442c338116758bd3acc1dc6b9d0
Matthew Khouzam <matthew.khouzam@ericsson.com>
2015-05-21 18:11:29 -0400

    tmf.swtbot: add x axis time alignment tests
    This test introduces SWTBotSash, it should be removed if mainlined into SWTBot
    
    Change-Id: I1ae6e460a2d59e07c955d9ccb98a961ffd53b0e2
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- org.eclipse.tracecompass.tmf.ui.swtbot.tests/shared/org/eclipse/tracecompass/tmf/ui/swtbot/tests/shared/SWTBotSash.java
+++ org.eclipse.tracecompass.tmf.ui.swtbot.tests/shared/org/eclipse/tracecompass/tmf/ui/swtbot/tests/shared/SWTBotSash.java
@@ -1,9 +1,11 @@
 package org.eclipse.tracecompass.tmf.ui.swtbot.tests.shared;
 
-import org.eclipse.swt.SWT;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Sash;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
@@ -19,9 +21,13 @@ public class SWTBotSash extends AbstractSWTBotControl<Sash> {
 
     /**
      * The widget wrapper
-     * @param w the sash
-     * @param description the description
-     * @throws WidgetNotFoundException if there is no widget
+     *
+     * @param w
+     *            the sash
+     * @param description
+     *            the description
+     * @throws WidgetNotFoundException
+     *             if there is no widget
      */
     public SWTBotSash(Sash w, SelfDescribing description) throws WidgetNotFoundException {
         super(w, description);
@@ -29,6 +35,7 @@ public class SWTBotSash extends AbstractSWTBotControl<Sash> {
 
     /**
      * Get the central point of the sash
+     *
      * @return the center point, good for dragging
      */
     public Point getPoint() {
@@ -37,35 +44,82 @@ public class SWTBotSash extends AbstractSWTBotControl<Sash> {
             @Override
             public Point run() {
                 Rectangle rect = widget.getBounds();
-                return new Point(rect.x + rect.width / 2, rect.y + rect.height / 2);
+                Point p = widget.toDisplay(0, 0);
+                return new Point(p.x + rect.width / 2, p.y + rect.height / 2);
             }
 
         });
-    }
-
-    private Event makeMouseEvent(Point p, boolean click) {
-        return createMouseEvent(p.x, p.y, 1, click ? SWT.BUTTON1 : null, click ? 1 : 0);
     }
 
     /**
      * Simulate a drag
-     * @param dst to this destination
+     *
+     * @param dst
+     *            to this destination
      */
     public void drag(final Point dst) {
         final Point src = getPoint();
-        UIThreadRunnable.asyncExec(new VoidResult() {
-            @Override
-            public void run() {
-                Event meMove = makeMouseEvent(src, false);
-                display.post(meMove);
-                Event meDown = makeMouseEvent(src, true);
-                display.post(meDown);
-                Event meMoveTarget = makeMouseEvent(dst, false);
-                display.post(meMoveTarget);
-                Event meUp = makeMouseEvent(dst, true);
-                display.post(meUp);
+        /*
+         * dn : MouseEvent{Sash {} time=262463957 data=null button=1
+         * stateMask=0x0 x=5 y=59 count=1}
+         *
+         * move : MouseEvent{Sash {} time=262464038 data=null button=0
+         * stateMask=0x80000 x=131 y=103 count=0}
+         *
+         * move : MouseEvent{Sash {} time=262464171 data=null button=0
+         * stateMask=0x80000 x=90 y=116 count=0}
+         *
+         * up : MouseEvent{Sash {} time=262464796 data=null button=1
+         * stateMask=0x80000 x=5 y=116 count=1}
+         */
+        try {
+            final Robot awtRobot = new Robot();
+
+            // move a maximum of 10 points / event
+            final int magDist = (src.x - dst.x) * (src.x - dst.x) + (src.y - dst.y) * (src.y - dst.y);
+
+            final int steps = (int) Math.sqrt( magDist / 100.0 ) ;
+
+            final int stepX = (dst.x - src.x) / steps;
+            final int stepY = (dst.y - src.y) / steps;
+            syncExec(new VoidResult() {
+                @Override
+                public void run() {
+                    awtRobot.mouseMove(src.x, src.y);
+                    SWTBotUtils.delay(15);
+                    awtRobot.mousePress(InputEvent.BUTTON1_MASK);
+
+                }
+            });
+            for (int i = 0; i < steps; i++) {
+                final int index = i;
+                syncExec(new VoidResult() {
+                    @Override
+                    public void run() {
+                        int x = src.x + index * stepX;
+                        int y = src.y + index * stepY;
+                        awtRobot.mouseMove(x, y);
+                    }
+                });
             }
-        });
+            asyncExec(new VoidResult() {
+                @Override
+                public void run() {
+                    awtRobot.mouseMove(dst.x, dst.y);
+                }
+            });
+            SWTBotUtils.delay(15);
+            syncExec(new VoidResult() {
+                @Override
+                public void run() {
+                    awtRobot.mouseRelease(InputEvent.BUTTON1_MASK);
+                }
+            });
+
+        } catch (final AWTException e) {
+            // log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 
 }
