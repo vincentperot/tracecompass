d0af442fd4bbe470a3fe89c3adbfdc46c2f99b2e
Patrick Tasse <patrick.tasse@gmail.com>
2015-06-12 14:40:54 -0400

    tmf: Bug 470057: Allow negative selection range in Histogram view
    This allows the selection start time to be higher than the selection end
    time. The Selection Start/End text control will no longer swap values
    when the user enters such values. Dragging a selection from right to
    left will result in a selection end time lower than start time.
    
    The link state of the selection text controls is now initialized to true
    by default.
    
    Change-Id: I1f735e98cf933bc20be69192d6063fe5c90f6776
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>

--- org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/histogram/Histogram.java
+++ org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/histogram/Histogram.java
@@ -707,11 +707,6 @@ public abstract class Histogram implements ControlListener, PaintListener, KeyLi
     // ------------------------------------------------------------------------
 
     private void updateSelectionTime() {
-        if (fSelectionBegin > fSelectionEnd) {
-            long end = fSelectionBegin;
-            fSelectionBegin = fSelectionEnd;
-            fSelectionEnd = end;
-        }
         ((HistogramView) fParentView).updateSelectionTime(fSelectionBegin, fSelectionEnd);
     }
 
