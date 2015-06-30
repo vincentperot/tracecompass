27741c47bb2fa8c910735c3a0bb0695059260540
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-06-29 21:50:53 -0400

    Analysis: Add the dependency graph builder module
    Change-Id: Icca0b33d07b14eee7009a0abd8fd064da061eed0
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.graph.core/src/org/eclipse/tracecompass/analysis/graph/core/building/AbstractTraceEventHandler.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2015 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Francis Giraldeau - Initial implementation and API
+ *   Geneviève Bastien - Initial implementation and API
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.analysis.graph.core.building;
+
+/**
+ * Base class for event handlers, implementing common behavior like cancellation
+ *
+ * @author Geneviève Bastien
+ * @author Francis Giraldeau
+ */
+public abstract class AbstractTraceEventHandler implements ITraceEventHandler {
+
+	boolean fHandlerCancelled = false;
+
+	@Override
+	public boolean isCancelled() {
+		return fHandlerCancelled;
+	}
+
+	@Override
+	public void cancel() {
+		fHandlerCancelled = true;
+	}
+
+}
