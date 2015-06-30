fbc3cc6d0852133b779f2194fa91e64eab2020b4
Geneviève Bastien <gbastien+lttng@versatic.net>
2015-06-29 22:16:12 -0400

    Analysis: Add unit tests for the critical path module
    Change-Id: If3c2cc85675314b5278c2dfedf4dd4dec554eaab
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>

--- analysis/org.eclipse.tracecompass.analysis.graph.core.tests/src/org/eclipse/tracecompass/analysis/graph/core/tests/analysis/criticalpath/AllTests.java
+++ analysis/org.eclipse.tracecompass.analysis.graph.core.tests/src/org/eclipse/tracecompass/analysis/graph/core/tests/analysis/criticalpath/AllTests.java
@@ -10,20 +10,20 @@
  *   Geneviève Bastien - Initial API and implementation
  *******************************************************************************/
 
-package org.eclipse.tracecompass.analysis.graph.core.tests;
+package org.eclipse.tracecompass.analysis.graph.core.tests.analysis.criticalpath;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
- * Master test suite for this plugin
+ * Test suite for the critical path package
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    org.eclipse.tracecompass.analysis.graph.core.tests.analysis.criticalpath.AllTests.class,
-    org.eclipse.tracecompass.analysis.graph.core.tests.graph.AllTests.class
+    TmfCriticalPathAlgoBoundedTest.class,
+    TmfCriticalPathAlgoUnboundedTest.class
 })
-public class AllAnalysisGraphCoreTests {
+public class AllTests {
 
 }
 
